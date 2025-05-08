f3c68f45a9904e3783f8cb259104bdbc96795ccd
Korbielowski <dawidstasinski12@gmail.com>
2025-05-08 22:19:40 +0200

    Start work on database and user creation form

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,40 @@
-from app_setup import app, templates
+from .app_setup import app, templates, engine
+from sqlmodel import Session
+from .models import (
+    ProfileInfoModel,
+    # Profile,
+    # Language,
+    # ProgrammingLanguage,
+    # Tool,
+    # Certificate,
+    # Charity,
+    # Education,
+    # Experience,
+    # Project,
+    # SocialPlatform,
+)
 from fastapi import Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 
 
 @app.get("/", response_class=HTMLResponse)
 # @authenticate_user
 async def root(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
+
+
+@app.get("/user_creation", response_class=HTMLResponse)
+async def load_user_form(request: Request):
+    return templates.TemplateResponse(request=request, name="user_creation.html")
+
+
+@app.post("/user_creation", response_class=RedirectResponse)
+async def create_user(request: Request, profile_information: ProfileInfoModel):
+    with Session(engine) as session:
+        info_validated = (
+            i.__class__.model_validate(i) for i in profile_information.model_dump()
+        )
+        session.add_all(info_validated)
+        session.commit()
+        # session.refresh()
+    return RedirectResponse(url=request.url_for("index"))
--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, DateTime, SQLModel
+from sqlmodel import Column, Field, DateTime, SQLModel, TIMESTAMP, text
 
 
 class Profile(SQLModel, table=True):
@@ -9,30 +9,58 @@ class Profile(SQLModel, table=True):
     age: int
 
 
+class ProfileModel(SQLModel):
+    name: str
+    middlename: str
+    surname: str
+    age: int
+
+
 class ProgrammingLanguage(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    profile_id: int | None = Field(default=None, foreign_key=True)
+    profile_id: int | None = Field(default=None, foreign_key="profile.id")
+    language: str
+    level: str  # Maybe in the future change to int
+
+
+class ProgrammingLanguageModel(SQLModel):
     language: str
     level: str  # Maybe in the future change to int
 
 
 class Language(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    profile_id: int | None = Field(default=None, foreign_key=True)
+    profile_id: int | None = Field(default=None, foreign_key="profile.id")
+    language: str
+    level: str  # Maybe in the future change to int
+
+
+class LanguageModel(SQLModel):
     language: str
     level: str  # Maybe in the future change to int
 
 
 class Tool(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    profile_id: int | None = Field(default=None, foreign_key=True)
+    profile_id: int | None = Field(default=None, foreign_key="profile.id")
+    name: str
+    level: str  # Maybe in the future change to int
+
+
+class ToolModel(SQLModel):
     name: str
     level: str  # Maybe in the future change to int
 
 
 class Certificate(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    profile_id: int | None = Field(default=None, foreign_key=True)
+    profile_id: int | None = Field(default=None, foreign_key="profile.id")
+    name: str
+    description: str
+    organisation: str
+
+
+class CertificateModel(SQLModel):
     name: str
     description: str
     organisation: str
@@ -40,7 +68,29 @@ class Certificate(SQLModel, table=True):
 
 class Charity(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    profile_id: int | None = Field(default=None, foreign_key=True)
+    profile_id: int | None = Field(default=None, foreign_key="profile.id")
+    name: str
+    description: str
+    organistaion: str
+    start_date: DateTime = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+            server_onupdate=text("CURRENT_TIMESTAMP"),
+        )
+    )
+    end_date: DateTime = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+            server_onupdate=text("CURRENT_TIMESTAMP"),
+        )
+    )
+
+
+class CharityModel(SQLModel):
     name: str
     description: str
     organistaion: str
@@ -50,27 +100,105 @@ class Charity(SQLModel, table=True):
 
 class Education(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    profile_id: int | None = Field(default=None, foreign_key=True)
+    profile_id: int | None = Field(default=None, foreign_key="profile.id")
     school: str
     major: str
     description: str
-    start_date: DateTime
-    end_date: DateTime
+    start_date: DateTime = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+            server_onupdate=text("CURRENT_TIMESTAMP"),
+        )
+    )
+    end_date: DateTime = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+            server_onupdate=text("CURRENT_TIMESTAMP"),
+        )
+    )
+
+
+class EducationModel(SQLModel):
+    school: str
+    major: str
+    description: str
+    start_date: DateTime = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+            server_onupdate=text("CURRENT_TIMESTAMP"),
+        )
+    )
+    end_date: DateTime = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+            server_onupdate=text("CURRENT_TIMESTAMP"),
+        )
+    )
 
 
 class Experience(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    profile_id: int | None = Field(default=None, foreign_key=True)
+    profile_id: int | None = Field(default=None, foreign_key="profile.id")
     company: str
     position: str
     description: str
-    start_date: DateTime
-    end_date: DateTime
+    start_date: DateTime = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+            server_onupdate=text("CURRENT_TIMESTAMP"),
+        )
+    )
+    end_date: DateTime = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+            server_onupdate=text("CURRENT_TIMESTAMP"),
+        )
+    )
+
+
+class ExperienceModel(SQLModel):
+    company: str
+    position: str
+    description: str
+    start_date: DateTime = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+            server_onupdate=text("CURRENT_TIMESTAMP"),
+        )
+    )
+    end_date: DateTime = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+            server_onupdate=text("CURRENT_TIMESTAMP"),
+        )
+    )
 
 
 class Project(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    profile_id: int | None = Field(default=None, foreign_key=True)
+    profile_id: int | None = Field(default=None, foreign_key="profile.id")
+    name: str
+    description: str
+    link: str
+
+
+class ProjectModel(SQLModel):
     name: str
     description: str
     link: str
@@ -78,6 +206,24 @@ class Project(SQLModel, table=True):
 
 class SocialPlatform(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    profile_id: int | None = Field(default=None, foreign_key=True)
+    profile_id: int | None = Field(default=None, foreign_key="profile.id")
     name: str
     link: str
+
+
+class SocialPlatformModel(SQLModel):
+    name: str
+    link: str
+
+
+class ProfileInfoModel(SQLModel):
+    profile: ProfileModel
+    programming_language: list[ProgrammingLanguageModel] | None
+    language: list[LanguageModel] | None
+    tool: list[ToolModel] | None
+    certificate: list[CertificateModel] | None
+    charity: list[CharityModel] | None
+    education: list[EducationModel] | None
+    experience: list[ExperienceModel] | None
+    project: list[ProjectModel] | None
+    social_platform: list[SocialPlatformModel] | None
