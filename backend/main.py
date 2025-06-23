35153561350fb1fa512ceab8b4e3d4ea129b3c15
Korbielowski <dawidstasinski12@gmail.com>
2025-06-23 22:17:27 +0200

    Fix redirect to test page

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,13 @@
 from .app_setup import app, templates
 from .models import ProfileInfoModel
-from fastapi import Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import Request, status
+from fastapi.responses import RedirectResponse
 
 
 @app.get("/")
 # @authenticate_user
 async def root(request: Request):
-    print("Elo")
-    return RedirectResponse(url=request.url_for("create_user"))
+    return RedirectResponse(url=request.url_for("load_user_form"))
     # return templates.TemplateResponse(request=request, name="index.html")
 
 
@@ -17,19 +16,23 @@ async def load_user_form(request: Request):
     return templates.TemplateResponse(request=request, name="create_user.html")
 
 
-@app.post("/create_user/")
-async def create_user(form_data: ProfileInfoModel):  # form: Annotated[TestInfo, Form()]
+@app.post("/create_user")
+async def create_user(
+    request: Request, form_data: ProfileInfoModel
+):  # form: Annotated[TestInfo, Form()]
     print(form_data)
     # with Session(app_setup.engine) as session:
     #     info_validated = (
     #         i.__class__.model_validate(i) for i in profile_information.model_dump()
     #     )
-    # session.add_all(info_validated)
-    # session.commit()
-    # session.refresh()
-    # return RedirectResponse(url=app.url_path_for("test"))
+    #     session.add_all(info_validated)
+    #     session.commit()
+    #     session.refresh()
+    return RedirectResponse(
+        url=request.url_for("test"), status_code=status.HTTP_303_SEE_OTHER
+    )
 
 
-@app.post("/test")
+@app.get("/test")
 async def test(request: Request):
-    return HTMLResponse("<h1>Hello</h1>")
+    return templates.TemplateResponse(request=request, name="index.html")
