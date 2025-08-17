e807a5889c9712c8cc90dd3d9b5c9fd38e20802e
Korbielowski <dawidstasinski12@gmail.com>
2025-08-17 22:04:41 +0200

    Add LLM scraper

---
+++ backend/scrapers/llm_scraper.py
@@ -0,0 +1,24 @@
+from .base_scraper import BaseScraper
+
+
+class LLMScraper(BaseScraper):
+    def __init__(self):
+        pass
+
+    def login_to_page(self) -> None:
+        pass
+
+    def get_job_entires(self):
+        pass
+
+    def go_to_next_page(self):
+        pass
+
+    def go_to_next_job(self):
+        pass
+
+    def apply_for_job(self):
+        pass
+
+    def process_job(self):
+        pass
