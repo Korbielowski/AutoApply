5ff677f8f0e2fb6325559e80047a7238a2185f20
Korbielowski <dawidstasinski12@gmail.com>
2025-08-21 13:48:23 +0200

    Fix __all__ in __init__.py

--- backend/scrapers/__init__.py
+++ backend/scrapers/__init__.py
@@ -1,1 +1,5 @@
-__all__ = ["linkedin", "pracuj_pl", "llm_scraper"]
+from .linkedin import LinkedIn
+from .pracuj_pl import PracujPl
+from .llm_scraper import LLMScraper
+
+__all__ = ["LinkedIn", "PracujPl", "LLMScraper"]
