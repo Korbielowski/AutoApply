91f6f024d09d774edd768d29f81dfe7cfc16ebd4
Korbielowski <dawidstasinski12@gmail.com>
2025-09-20 17:08:28 +0200

    Update LLMScraper

--- backend/scrapers/llm_scraper.py
+++ backend/scrapers/llm_scraper.py
@@ -1,16 +1,29 @@
 from playwright.async_api import Locator
+from openai import RateLimitError
+from bs4 import BeautifulSoup
 from loguru import logger
+import tiktoken
 
-from .base_scraper import BaseScraper, JobEntry
-from ..llm import send_req_to_llm
+from backend.scrapers.base_scraper import BaseScraper, JobEntry
+from backend.llm import send_req_to_llm
 
 import json
+import asyncio
+
+TIK = tiktoken.encoding_for_model("gpt-5-")
 
 
+# TODO: Save all important information about the pages in database (e.g. links, buttons, fields, scrollbars)
+# for later use, so that we don't make so many requests to LLM if we already know certain page.
+# If saved information does not work at any stage of the process, switch back to LLM scraper and update adequate steps in database
 class LLMScraper(BaseScraper):
     async def login_to_page(self) -> None:
         await self.page.goto(self.link)
         await self.page.wait_for_load_state("load")
+        # TODO: Make a loop out of this code
+        await self._pass_cookies_popup()
+        if not await self._is_on_login_page():
+            await self._navigate_to_login_page()
 
         email_field_locator = await self.find_html_element(
             "Find the login input field for username/email."
@@ -32,16 +45,67 @@ class LLMScraper(BaseScraper):
             )
             return
 
+        logger.info(
+            f"{self.link}, {email_field_locator=}, {password_field_locator=}, {sign_in_btn_locator=}"
+        )
         await email_field_locator.fill(self.email)
         await password_field_locator.fill(self.password)
         await sign_in_btn_locator.click()
         await self.page.wait_for_load_state("load")
+        # await asyncio.sleep(10)
 
+    async def _is_on_login_page(self) -> bool:
+        url = self.page.url
+        if "login" in url or "signin" in url or "sign-in" in url or "sign_in" in url:
+            return True
+
+        if "True" in send_req_to_llm(
+            f"Determine if this site is a login page, return only True or False: {await self._get_page_content()}",
+            use_openai=True,
+        ):
+            return True
+
+        return False
+
+    async def _navigate_to_login_page(self) -> None:
+        btn = await self.find_html_element("Find a button that opens login page")
+        if not btn:
+            logger.error("Could not find button to login page")
+            return
+        logger.info(f"Found login button: {btn}\n{await btn.all_inner_texts()}")
+        await btn.click()
+        await self.page.wait_for_load_state("load")
+
+    async def _pass_cookies_popup(self) -> None:
+        # TODO: Try using cookies to avoid popups
+        btn = await self.find_html_element(
+            "Find button responsible for accepting website cookies"
+        )
+        if not btn:
+            logger.error("Could not find cookies button")
+            return
+        logger.info(f"Found cookies button: {btn}\n{await btn.all_inner_texts()}")
+        await btn.click()
+        await self.page.wait_for_load_state("load")
+
+    # TODO: Change this method name to "_navigate_to_job_list"
     async def _go_to_job_list(self) -> None:
-        pass
+        btn = self.find_html_element("Find button that opens job list")
+        if not btn:
+            logger.error("Could not find job list button")
+            return
+        logger.info(f"Found job list button: {btn}\n{await btn.all_inner_texts()}")
+        await btn.click()
+        await self.page.wait_for_load_state("load")
 
     async def get_job_entires(self) -> tuple[Locator, ...]:
-        pass
+        scroll = self.find_html_element(
+            "Find scrollbar element that is responsible for displaying more job entries"
+        )
+        if not scroll:
+            logger.error("Could not find job list button")
+            return tuple()
+        return tuple()
 
     async def go_to_next_page(self) -> bool:
         pass
@@ -58,10 +122,32 @@ class LLMScraper(BaseScraper):
     async def find_html_element(self, prompt: str) -> None | Locator:
         pre_prompt = "I will give you an HTML snippet."
         post_prompt = "Return only its identifying attributes in JSON format with the following keys: id, name, type, aria-label, placeholder, role, text, classList. If an attribute does not exist, return null for it. Do not explain, only return JSON"
-        page_content = (
-            await self.page.content()
-        )  # TODO: Make page content smaller by e.g. excluding head or code tags
-        response = send_req_to_llm(f"{pre_prompt}{prompt}{post_prompt}\n{page_content}")
+        page_content = await self._get_page_content()
+        prompt = f"{pre_prompt}{prompt}{post_prompt}\n{page_content}"
+
+        try:
+            response = send_req_to_llm(
+                prompt,
+                use_openai=True,
+            )
+        except RateLimitError as e:
+            time = (
+                e.response.headers.get("x-ratelimit-reset-tokens", "")
+                .replace("m", "m ")
+                .replace("s", "")
+                .split(" ")
+            )
+            delay = 0
+
+            if "m" in time[0]:
+                delay += int(time.replace("m", "")) * 60
+            delay += int(time[-1])
+
+            await asyncio.sleep(delay)
+            response = send_req_to_llm(
+                prompt,
+                use_openai=True,
+            )
 
         try:
             attributes = json.loads(response)
@@ -70,35 +156,36 @@ class LLMScraper(BaseScraper):
             logger.exception(e)
             return None
 
+        # TODO: Make sure that we are selecting only one element, especially true for "type" and "classList"
         id = attributes.get("id", "")
         locator = self.page.locator(f"#{id}")
-        if locator.count() != 0:
-            return locator
-
-        name = attributes.get("name", "")
-        locator = self.page.locator(f'[name="{name}"]')
-        if locator.count() != 0:
-            return locator
-
-        type = attributes.get("type", "")
-        locator = self.page.locator(f'[type="{type}"]')
-        if locator.count() != 0:
-            return locator
-
-        aria_label = attributes.get("aria-label", "")
-        locator = self.page.get_by_label(aria_label)
-        if locator.count() != 0:
-            return locator
-
-        placeholder = attributes.get("placeholder", "")
-        locator = self.page.get_by_placeholder(placeholder)
-        if locator.count() != 0:
-            return locator
+        if await locator.count() != 0:
+            return locator.last
 
         text = attributes.get("text", "")
         locator = self.page.get_by_text(text)
-        if locator.count() != 0:
-            return locator
+        if await locator.count() != 0:
+            return locator.last
+
+        aria_label = attributes.get("aria-label", "")
+        locator = self.page.get_by_label(aria_label)
+        if await locator.count() != 0:
+            return locator.last
+
+        name = attributes.get("name", "")
+        locator = self.page.locator(f'[name="{name}"]')
+        if await locator.count() != 0:
+            return locator.last
+
+        type = attributes.get("type", "")
+        locator = self.page.locator(f'[type="{type}"]')
+        if await locator.count() != 0:
+            return locator.last
+
+        # placeholder = attributes.get("placeholder", "")
+        # locator = self.page.get_by_placeholder(placeholder)
+        # if await locator.count() != 0:
+        #     return locator
 
         # class_list = attributes.get("classList", "")
         # locator = self.page.get_by()
@@ -107,3 +194,38 @@ class LLMScraper(BaseScraper):
         # locator = self.page.get_by_role()
 
         return None
+
+    # TODO: Move this method to BaseScraper class as it can be also used in other derived classes
+    async def _get_page_content(self) -> str:
+        # TODO: Make page content smaller by e.g. excluding head or code tags and only by including body
+        # TODO: Check in the future, whether regex would not be faster and overall better solution
+        page_content = await self.page.content()
+        soup = BeautifulSoup(page_content, "html.parser")
+        for tag in soup(
+            [
+                "head",
+                "meta",
+                "style",
+                "script",
+                "noscript",
+                "template",
+                "iframe",
+                "video",
+                "audio",
+                "map",
+                "area",
+                "embed",
+                "object",
+                "applet",
+                "track",
+                "canvas",
+                "svg",
+                "img",
+                "picture",
+                "source",
+            ]
+        ):
+            tag.decompose()
+            cleaned_page_content = str(soup)
+            logger.info(f"Amount of tokens: {len(TIK.encode(cleaned_page_content))}")
+        return cleaned_page_content
