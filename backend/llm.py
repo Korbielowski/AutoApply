0b0cff7ac95d9a7a791472bf4d6baff66c9e3e57
Korbielowski <dawidstasinski12@gmail.com>
2025-09-20 17:09:29 +0200

    Update send_req_to_llm function

--- backend/llm.py
+++ backend/llm.py
@@ -1,6 +1,6 @@
 # TODO: If not used, remove openaia-agents from dependencies and add normal OpenAI
 # TODO: Remove dotenv and code related to it from this file, when app setup works fine
-from openai import OpenAI
+from openai import OpenAI, RateLimitError, AuthenticationError
 from dotenv import load_dotenv
 from loguru import logger
 
@@ -9,19 +9,38 @@ import os
 # TODO: Uncomment this in the future: from app_setup import API_KEY
 load_dotenv()
 API_KEY = os.getenv("API_KEY", "")
-LLM = OpenAI(base_url="https://api.llm7.io/v1", api_key="unused")
+OPEAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+BASE_URL = "https://api.llm7.io/v1"
 MODEL = "deepseek-r1-0528"
+OPENAI_MODEL = "gpt-5-nano-2025-08-07"
 
 
-def send_req_to_llm(prompt: str, temperature: float = 1) -> str:
+def send_req_to_llm(
+    prompt: str, temperature: float = 1, use_openai: bool = False
+) -> str:
     # response = LLM.responses.create(model="deepseek/deepseek-r1-distill-llama-70b:free", instructions=prompt, input=str(description))
-    try:
-        completion = LLM.chat.completions.create(
-            model=MODEL,
-            messages=[{"role": "user", "content": prompt}],
-            temperature=temperature,
-        )
-        return completion.choices[0].message.content
-    except Exception as e:
-        logger.info(f"LLM error: {e}")
+    if use_openai:
+        client = OpenAI(api_key=OPEAI_API_KEY)
+        try:
+            response = client.responses.create(
+                model=OPENAI_MODEL, input=prompt, temperature=temperature
+            )
+            return response.output_text
+        except RateLimitError as e:
+            logger.info(f"LLM error: {e}")
+            raise e
+        except AuthenticationError as e:
+            logger.info(f"LLM error: {e}")
+    else:
+        client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
+        try:
+            response = client.responses.create(
+                model=MODEL,
+                input=prompt,
+                # messages=[{"role": "user", "content": prompt}],
+                temperature=temperature,
+            )
+            return response.output_text
+        except Exception as e:
+            logger.info(f"LLM error: {e}")
     return ""
