04177b204c87f19accdc93c61686525957be165f
Korbielowski <dawidstasinski12@gmail.com>
2025-09-22 19:31:04 +0200

    Move RateLimitError exception handling to send_req_to_llm function

--- backend/llm.py
+++ backend/llm.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from loguru import logger
 
 import os
+import asyncio
 
 # TODO: Uncomment this in the future: from app_setup import API_KEY
 load_dotenv()
@@ -15,7 +16,7 @@ MODEL = "deepseek-r1-0528"
 OPENAI_MODEL = "gpt-5-nano-2025-08-07"
 
 
-def send_req_to_llm(
+async def send_req_to_llm(
     prompt: str, temperature: float = 1, use_openai: bool = False
 ) -> str:
     # response = LLM.responses.create(model="deepseek/deepseek-r1-distill-llama-70b:free", instructions=prompt, input=str(description))
@@ -28,7 +29,25 @@ def send_req_to_llm(
             return response.output_text
         except RateLimitError as e:
             logger.info(f"LLM error: {e}")
-            raise e
+            logger.error("Too many tokens in 1 minute")
+            time = (
+                e.response.headers.get("x-ratelimit-reset-tokens", "")
+                .replace("m", "m ")
+                .replace("s", "")
+                .split(" ")
+            )
+            delay = 0
+
+            if "m" in time[0]:
+                delay += int(time.replace("m", "")) * 60
+            delay += int(time[-1])
+
+            logger.info(f"Sleeping for: {delay} seconds")
+            await asyncio.sleep(delay)
+            response = send_req_to_llm(
+                prompt,
+                use_openai=True,
+            )
         except AuthenticationError as e:
             logger.info(f"LLM error: {e}")
     else:
