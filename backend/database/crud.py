28b89c3b9d28cd86af5f84a6e7b42214020906a9
Korbielowski <dawidstasinski12@gmail.com>
2025-10-04 18:05:40 +0200

    Change argument type form User to UserModel

--- backend/database/crud.py
+++ backend/database/crud.py
@@ -1,9 +1,9 @@
 from sqlmodel import Session, select
 
-from backend.database.models import User, UserModel
+from backend.database.models import UserModel
 
 
-def create_user(session: Session, user: User) -> UserModel:
+def create_user(session: Session, user: UserModel) -> UserModel:
     session.add(user)
     session.commit()
     return session.exec(select(UserModel).where(UserModel.email == user.email)).first()
