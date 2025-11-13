ac10b740a462fa543af80a10d3cf57eeef0e3b01
Korbielowski <dawidstasinski12@gmail.com>
2025-11-13 14:11:07 +0100

    Configure logger

--- backend/logging.py
+++ backend/logging.py
@@ -1,7 +1,46 @@
+import sys
+
 from loguru import logger
+
+from backend.config import settings
+
+LOGS_FORMAT = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+    "<level>{level: <8}</level> | "
+    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+)
+
+logger.remove(0)
+if settings.DEBUG:
+    logger.add(
+        sink=sys.stderr,
+        level="DEBUG",
+        format=LOGS_FORMAT,
+        colorize=True,
+    )
+else:
+    logger.add(
+        sink=sys.stderr,
+        level="INFO",
+        format=LOGS_FORMAT,
+        colorize=True,
+    )
+logger.add(sink=f"{settings.PROJECT_NAME}.log", level="DEBUG", colorize=False)
+
 
 logger.level(
     name="FAILURE",
     no=25,
     color="<red>",
 )
+
+
+def __log_failure(self, message, *args, **kwargs):
+    self.log("FAILURE", message, *args, **kwargs)
+
+
+logger.failure = __log_failure.__get__(logger, logger.__class__)
+
+
+def get_logger():
+    return logger
