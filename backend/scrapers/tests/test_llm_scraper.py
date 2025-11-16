203b999a00bffcfd116ef2edff86c99bd568ab4f
Korbielowski <dawidstasinski12@gmail.com>
2025-11-16 17:08:33 +0100

    Update tests for LLMScraper class

--- backend/scrapers/tests/test_llm_scraper.py
+++ backend/scrapers/tests/test_llm_scraper.py
@@ -5,8 +5,9 @@ from loguru import logger
 from playwright.async_api import async_playwright
 from playwright_stealth import Stealth
 
-from backend.database.models import ProfileModel
-from backend.scrapers import LLMScraper
+from backend.config import settings
+from backend.database.models import UserModel
+from backend.scrapers.llm_scraper import LLMScraper
 
 
 @pytest.mark.asyncio
@@ -15,21 +16,19 @@ async def test_is_on_login_page_return_false_when_not_on_login_page(
     get_data_for_scraper,
 ):
     async with Stealth().use_async(async_playwright()) as playwright:
-        playwright.selectors.set_test_id_attribute("data-control-id")
         browser = await playwright.chromium.launch(headless=False)
-        page = await browser.new_page(locale="en-US")
-        profile, email, password = get_data_for_scraper
-        link = "https://pracuj.pl/"
+        context = await browser.new_context(locale="en-US")
+        page = await context.new_page()
+        url = "https://it.pracuj.pl/praca"
         scraper = LLMScraper(
-            link=link,
-            profile=profile,
-            email=email,
-            password=password,
+            url=url,
+            email=settings.USER_EMAIL,
+            password=settings.PASSWORD,
+            context=context,
             page=page,
-            auto_apply=False,
-            generate_cv=False,
+            website_info=None,
         )
-        await scraper.page.goto(scraper.link)
+        await scraper.page.goto(scraper.url)
         await scraper.page.wait_for_load_state("load")
         assert await scraper._is_on_login_page() is False
 
@@ -40,21 +39,19 @@ async def test_is_on_login_page_return_true_when_on_login_page(
     get_data_for_scraper,
 ):
     async with Stealth().use_async(async_playwright()) as playwright:
-        playwright.selectors.set_test_id_attribute("data-control-id")
         browser = await playwright.chromium.launch(headless=False)
-        page = await browser.new_page(locale="en-US")
-        profile, email, password = get_data_for_scraper
-        link = "https://login.pracuj.pl/"
+        context = await browser.new_context(locale="en-US")
+        page = await context.new_page()
+        url = "https://it.pracuj.pl/praca"
         scraper = LLMScraper(
-            link=link,
-            profile=profile,
-            email=email,
-            password=password,
+            url=url,
+            email=settings.USER_EMAIL,
+            password=settings.PASSWORD,
+            context=context,
             page=page,
-            auto_apply=False,
-            generate_cv=False,
+            website_info=None,
         )
-        await scraper.page.goto(scraper.link)
+        await scraper.page.goto(scraper.url)
         await scraper.page.wait_for_load_state("load")
         assert await scraper._is_on_login_page() is True
 
@@ -63,21 +60,19 @@ async def test_is_on_login_page_return_true_when_on_login_page(
 @pytest.mark.skip
 async def test_navigate_to_login_page(get_data_for_scraper, caplog):
     async with Stealth().use_async(async_playwright()) as playwright:
-        playwright.selectors.set_test_id_attribute("data-control-id")
         browser = await playwright.chromium.launch(headless=False)
-        page = await browser.new_page(locale="en-US")
-        profile, email, password = get_data_for_scraper
-        link = "https://pracuj.pl/"
+        context = await browser.new_context(locale="en-US")
+        page = await context.new_page()
+        url = "https://it.pracuj.pl/praca"
         scraper = LLMScraper(
-            link=link,
-            profile=profile,
-            email=email,
-            password=password,
+            url=url,
+            email=settings.USER_EMAIL,
+            password=settings.PASSWORD,
+            context=context,
             page=page,
-            auto_apply=False,
-            generate_cv=False,
+            website_info=None,
         )
-        await scraper.page.goto(scraper.link)
+        await scraper.page.goto(scraper.url)
         await scraper.page.wait_for_load_state("load")
         with caplog.at_level(logging.ERROR):
             await scraper._navigate_to_login_page()
@@ -88,74 +83,74 @@ async def test_navigate_to_login_page(get_data_for_scraper, caplog):
 @pytest.mark.skip
 async def test_loging_to_page(get_data_for_scraper, caplog):
     async with Stealth().use_async(async_playwright()) as playwright:
-        playwright.selectors.set_test_id_attribute("data-control-id")
         browser = await playwright.chromium.launch(headless=False)
-        page = await browser.new_page(locale="en-US")
-        profile, email, password = get_data_for_scraper
-        link = "https://pracuj.pl/"
+        context = await browser.new_context(locale="en-US")
+        page = await context.new_page()
+        url = "https://it.pracuj.pl/praca"
         scraper = LLMScraper(
-            link=link,
-            profile=profile,
-            email=email,
-            password=password,
+            url=url,
+            email=settings.USER_EMAIL,
+            password=settings.PASSWORD,
+            context=context,
             page=page,
-            auto_apply=False,
-            generate_cv=False,
+            website_info=None,
         )
         with caplog.at_level(logging.ERROR):
             await scraper.login_to_page()
             assert "Could not log into" not in caplog.text
 
 
-@pytest.mark.asyncio
-@pytest.mark.skip
-async def test_get_page_content(get_data_for_scraper, caplog):
-    async with Stealth().use_async(async_playwright()) as playwright:
-        playwright.selectors.set_test_id_attribute("data-control-id")
-        browser = await playwright.chromium.launch(headless=False)
-        page = await browser.new_page(locale="en-US")
-        profile, email, password = get_data_for_scraper
-        link = "https://pracuj.pl/"
-        scraper = LLMScraper(
-            link=link,
-            profile=profile,
-            email=email,
-            password=password,
-            page=page,
-            auto_apply=False,
-            generate_cv=False,
-        )
-        await scraper.page.goto(scraper.link)
-        await scraper.page.wait_for_load_state("load")
-        content = await scraper._get_page_content()
-        logger.info(type(content))
-        assert type(content) is not str
+# @pytest.mark.asyncio
+# @pytest.mark.skip
+# async def test_get_page_content(get_data_for_scraper, caplog):
+#     async with Stealth().use_async(async_playwright()) as playwright:
+#         browser = await playwright.chromium.launch(headless=False)
+#         context = await browser.new_context(locale="en-US")
+#         page = await context.new_page()
+#         url = "https://it.pracuj.pl/praca"
+#         scraper = LLMScraper(
+#             url=url,
+#             email=settings.USER_EMAIL,
+#             password=settings.PASSWORD,
+#             context=context,
+#             page=page,
+#             website_info=None,
+#         )
+#         await scraper.page.goto(scraper.url)
+#         await scraper.page.wait_for_load_state("load")
+#         content = await scraper._get_page_content()
+#         logger.info(type(content))
+#         assert type(content) is not str
 
 
 @pytest.mark.asyncio
 async def test_get_job_entries(get_data_for_scraper):
     async with Stealth().use_async(async_playwright()) as playwright:
-        playwright.selectors.set_test_id_attribute("data-control-id")
         browser = await playwright.chromium.launch(headless=False)
-        page = await browser.new_page(locale="en-US")
-        profile, email, password = get_data_for_scraper
-        link = "https://it.pracuj.pl/praca"
+        context = await browser.new_context(locale="en-US")
+        page = await context.new_page()
+        url = "https://it.pracuj.pl/praca"
         scraper = LLMScraper(
-            link=link,
-            profile=profile,
-            email=email,
-            password=password,
+            url=url,
+            email=settings.USER_EMAIL,
+            password=settings.PASSWORD,
+            context=context,
             page=page,
-            auto_apply=False,
-            generate_cv=False,
+            website_info=None,
         )
-        await scraper.page.goto(scraper.link)
+        await scraper.page.goto(scraper.url)
         await scraper.page.wait_for_load_state("load")
         await scraper._pass_cookies_popup()
-        job_entries = await scraper.get_job_entires()
-        logger.info(f"Amount of jobs from page: {len(job_entries)}")
+        job_entries = await scraper.get_job_entries()
         for job in job_entries:
-            logger.info(await job.all_text_contents())
+            logger.info(f"Now Scraping: {await job.get_attribute('href')}")
+            entry = await scraper._get_job_information(
+                await job.get_attribute("href")
+            )
+            if entry:
+                logger.info("Everything works fine for now")
+            else:
+                logger.error("Something is wrong :(")
         assert type(job_entries) is tuple
         assert len(job_entries) > 0
 
@@ -163,8 +158,8 @@ async def test_get_job_entries(get_data_for_scraper):
 @pytest.fixture(
     scope="module",
 )
-def get_data_for_scraper() -> tuple[ProfileModel, str, str]:
-    profile = ProfileModel(
+def get_data_for_scraper() -> tuple[UserModel, str, str]:
+    profile = UserModel(
         firstname="test", middlename="test", surname="test", age="30"
     )
     email = "test.test@gmail.com"
