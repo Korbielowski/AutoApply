9f1b9ad42c1a2126ea45e40fc3a68e070fde5700
Korbielowski <dawidstasinski12@gmail.com>
2025-11-26 22:29:29 +0100

    Add lru_cache, handle longer paths

--- backend/utils.py
+++ backend/utils.py
@@ -1,8 +1,11 @@
+from functools import lru_cache
+
 import aiofiles
 import yaml
 
 
-# TODO: Make this fully async
+# TODO: Make this function fully async
+@lru_cache()
 async def load_prompt(prompt_path: str, **kwargs) -> str:
     paths = prompt_path.split(":")
     if len(paths) < 2:
@@ -10,13 +13,19 @@ async def load_prompt(prompt_path: str, **kwargs) -> str:
             "prompt_path parameter should be specified as this 'example:example'"
         )
 
+    data: dict = {}
     async with aiofiles.open(paths[0], "r") as file:
         file_content = await file.read()
-        data: dict = yaml.safe_load(file_content)
+        data = yaml.safe_load(file_content)
         if type(data) is not dict:
             raise Exception(f"Bad yaml structure in this file: {paths[0]}")
 
-    prompt = data.get(paths[1], "")
-    if kwargs:
+    for p in paths[1:]:
+        data = data.get(p, {})
+
+    prompt = data.get("prompt", "")
+    params = data.get("params", [])
+
+    if kwargs and params:
         prompt = prompt.format_map(**kwargs)
     return prompt
