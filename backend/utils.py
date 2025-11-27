0a2d315ad1c4351fe5422a11c1795adcfbf41cb4
Korbielowski <dawidstasinski12@gmail.com>
2025-11-27 16:49:34 +0100

    Add better error handling

--- backend/utils.py
+++ backend/utils.py
@@ -3,6 +3,8 @@ from functools import lru_cache
 import aiofiles
 import yaml
 
+from backend.config import settings
+
 
 # TODO: Make this function fully async
 @lru_cache()
@@ -14,7 +16,8 @@ async def load_prompt(prompt_path: str, **kwargs) -> str:
         )
 
     data: dict = {}
-    async with aiofiles.open(paths[0], "r") as file:
+    prompt_file_path = f"{settings.ROOT_DIR}/prompts/{paths[0]}.yaml"
+    async with aiofiles.open(prompt_file_path, "r") as file:
         file_content = await file.read()
         data = yaml.safe_load(file_content)
         if type(data) is not dict:
@@ -26,6 +29,14 @@ async def load_prompt(prompt_path: str, **kwargs) -> str:
     prompt = data.get("prompt", "")
     params = data.get("params", [])
 
-    if kwargs and params:
-        prompt = prompt.format_map(**kwargs)
+    if not prompt:
+        raise Exception(
+            f"Some error occurred while retrieving 'prompt' key from {prompt_file_path} and specified prompt path: {prompt_path}"
+        )
+
+    if params:
+        try:
+            prompt = prompt.format_map(kwargs)
+        except KeyError as e:
+            raise Exception(f"Keyword argument/s missing: {e.args}")
     return prompt
