a9ef3b99ae570772158f0f57cd9c9a7068d104c2
Korbielowski <dawidstasinski12@gmail.com>
2025-11-28 22:41:48 +0100

    Use Path operator instead of string

--- backend/prompts.py
+++ backend/prompts.py
@@ -16,7 +16,7 @@ async def load_prompt(prompt_path: str, **kwargs) -> str:
         )
 
     data: dict = {}
-    prompt_file_path = f"{settings.ROOT_DIR}/prompts/{paths[0]}.yaml"
+    prompt_file_path = settings.ROOT_DIR / "prompts" / f"{paths[0]}.yaml"
     async with aiofiles.open(prompt_file_path, "r") as file:
         file_content = await file.read()
         data = yaml.safe_load(file_content)
