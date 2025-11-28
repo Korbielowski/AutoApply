ae847b22832cba98377fc8c34ffa124ebc9d6073
Korbielowski <dawidstasinski12@gmail.com>
2025-11-28 22:42:44 +0100

    Use Path object instead of string

--- backend/config.py
+++ backend/config.py
@@ -1,19 +1,20 @@
 import os
+from pathlib import Path
 
 from pydantic import PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+_ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=f"{_ROOT_DIR}/.env",
+        env_file=_ROOT_DIR / ".env",
         env_ignore_empty=True,
         extra="ignore",
     )
     PROJECT_NAME: str = "AutoApply"
-    ROOT_DIR: str = _ROOT_DIR
+    ROOT_DIR: Path = _ROOT_DIR
     DEBUG: bool = False
     LOG_TO_FILE: bool = True
     USER_EMAIL: str
